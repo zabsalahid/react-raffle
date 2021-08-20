06e95abc09887f50d62e0d92c03028647fc5cb50
Zab Salahid <zabsalahid@gmail.com>
2021-08-20 23:08:22 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth={false}>
+      <Grid container>
+        <Grid item xs={12}>
+          Test
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
